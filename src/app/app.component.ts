7774de39a7de49b3de22139848bf8444089677ae
buckapi <buckapis@gmail.com>
2023-10-26 07:02:54 -0500

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScriptService } from './services/script.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angeleshadows';
+  constructor(
+    public script: ScriptService
+  )
+  {
+    this.script.load(
+      'jquery',
+      'selectize',
+      'plugins',
+      'main'
+                )
+          .then(data => {
+            // this.yeoman.isLoaded=true;
+          })
+          .catch(error => console.log(error)); 
+    // setTheme('');
+  }
 }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ScriptService } from '@app/services/script.service';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    public script: ScriptService
+  ) {
+    this.script.load(
+      'jquery',
+      'selectize',
+      'plugins',
+      'main'
+                )
+          .then(data => {
+            // this.yeoman.isLoaded=true;
+          })
+          .catch(error => console.log(error)); 
+    // setTheme('');
+   }
+
+  ngOnInit(): void {
+  }
+
+}
